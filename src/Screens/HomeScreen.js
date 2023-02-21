1b3c338d6a09c5cbd0de3de5f51cd8acdf3eb147
ArecheBrian <arecheminayabj@gmail.com>
2023-02-21 00:17:22 -0400

    all tasks of the last sprint

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useSelector, useDispatch } from 'react-redux';
+import { addApellido } from '../Redux/Features/testSlice';
+
 
 export const HomeScreen = () => {
+  const name = useSelector((State) => State.name.name)
+  console.log(name)
+  const dispatch = useDispatch()
   return (
     <View style={styles.container}>
       <Text>Home</Text>
+      <TouchableOpacity style={{ backgroundColor: "red"}} onPress={()=> dispatch(addApellido())}>
+        <Text>klk manito {name}</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
