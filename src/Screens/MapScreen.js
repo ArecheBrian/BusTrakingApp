4fb1c70b503c47b9e8d0f40fe14befffe391a1de
2020-10697 <101667028+EESS12@users.noreply.github.com>
2023-02-25 19:13:03 -0400

    add

--- src/Screens/MapScreen.js
+++ src/Screens/MapScreen.js
@@ -1,25 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps";
 
 export const MapScreen = () => {
   return (
     <View style={styles.container}>
-     <MapView style={styles.map}/>
-     <StatusBar style="auto" />
+      <StatusBar style="auto" />
+      <MapView style={styles.map} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   map: {
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
   },
 });
