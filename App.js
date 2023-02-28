c0ae8513bb39560d1f5574662ae5daeeb05e725d
ArecheBrian <arecheminayabj@gmail.com>
2023-02-27 23:49:57 -0400

    home UI desing

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-import { Navigation } from './src/Navigation/navigation';
 import 'react-native-gesture-handler';
+import { BusTrakingApp } from "./src/Navigation/navigation"
 import { store } from './src/Redux/Store/Store';
 import { Provider } from 'react-redux';
+import { NativeBaseProvider } from 'native-base';
+import theme from './src/Constans/theme';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Navigation/>
+      <NativeBaseProvider theme={theme}>
+        <BusTrakingApp/>
+      </NativeBaseProvider>
     </Provider>
   );
 }
--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,41 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { useSelector, useDispatch } from 'react-redux';
-import { addApellido } from '../Redux/Features/testSlice';
+import { VStack, ScrollView, Text, useColorModeValue, useSafeArea, StatusBar } from "native-base"
+import { HomeHeader } from "../Components/HomeComponents/HomeHeader";
+import { HomeMap } from "../Components/HomeComponents/HomeMap";
 
-
+const SafeArea =()=> {
+    const safeAreaProps = useSafeArea({
+      safeAreaTop: true,
+    });
+    return (
+        <VStack _dark={{bg: "blueGray.900"}} _light={{bg: "blueGray.50"}} flex={1} {...safeAreaProps} alignItems="center">
+          <HomeHeader/>
+          <HomeMap />
+        </VStack>
+    );
+  }
+  
+  
 export const HomeScreen = () => {
-  const name = useSelector((State) => State.name.name)
-  const ticket = useSelector ((State) => State.tickets)
-  const Users = useSelector ((State) => State.Users)
-  console.log(name)
-  const dispatch = useDispatch()
-  return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text style={{fontSize: 27, fontWeight: 'bold' , padding: 40, }}> Bienvenido {Users.fullName}</Text>
-      <Text>Home</Text>
-      <TouchableOpacity style={{ backgroundColor: "red"}} onPress={()=> dispatch(addApellido())}>
-        <Text>klk manito {name}</Text>
-      </TouchableOpacity>
-
-      <Text style={{fontSize: 17, fontWeight: 'bold' , marginTop: "5%" }}>Prueba de Tickets</Text>
-      <Text>{ticket.price}</Text>
-      <Text>{ticket.quantity}</Text>
-      <Text>{ticket.routeName}</Text>
-      <Text>{ticket.travelDate}</Text>
-      <Text>{ticket.hourDate}</Text>
-
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    return <SafeArea />
+  }
